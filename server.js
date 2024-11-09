23c13a9b83720923adec198e17c0d855a0044fba
Ante <ante.petrovic1002@gmail.com>
2024-11-09 13:48:53 +0100

    site

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
